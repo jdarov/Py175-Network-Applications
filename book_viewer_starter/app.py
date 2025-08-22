efc4e0147aeb4ce98e68da8bca542c54c9a46518
Joshua Darovitz <joshdarovitz@gmail.com>
2025-08-22 01:16:36 -0400

    Added book_viewer for FLASK web development practice

---
+++ book_viewer_starter/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    with open('book_viewer/data/toc.txt', 'r') as file:
+        contents = file.readlines()
+        
+    return render_template('index.html', 
+                           title="Book Viewer", 
+                           contents=contents)
+
+@app.route("/chapters/1")
+def chapter1():
+    chapter_title = "Chapter 1"
+    with open('book_viewer/data/toc.txt', 'r') as file:
+        contents = file.readlines()
+
+    with open('book_viewer/data/chp1.txt', 'r') as file:
+        chapter = file.read()
+    
+    return render_template('chapter.html',
+                           chapter_title=chapter_title,
+                           contents=contents,
+                           chapter=chapter)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
